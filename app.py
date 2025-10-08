19e0fd0111d8e714a68213ebbda4d76175addebc
hooke-naoto <42460482+hooke-naoto@users.noreply.github.com>
2025-10-08 23:12:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("TAO63 団員の配布数ランキング from teket CSV")
 
-f = st.file_uploader("販売履歴CSVを選択", type=["csv"])
+f = st.file_uploader("teket CSV (販売履歴) をアップロード", type=["csv"])
 if f:
     df = pd.read_csv(f, encoding="cp932")
     df = df[(df["クーポン"]=="TAO1013gregor") & ~df["処理"].isin(["無料受取","有料受取"])]
@@ -13,4 +13,5 @@ if f:
     out["合計枚数"] = out["S席"] + out["A席"]
     st.dataframe(out.sort_values("合計枚数", ascending=False))
     st.download_button("CSVダウンロード", out.to_csv().encode("utf-8-sig"), file_name="集計結果.csv")
+
 
